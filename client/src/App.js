ba5cf86d155468753f0fdbf2e3e3b1f04639e4b4
William Rozier <wrozier@princesscruises.com>
2024-07-24 09:36:38 -0400

    File correction

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
-// client/src/App.js
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
 import Login from './pages/Login';
-import Register from './pages/Register';
+import Home from './pages/Home';
 import ProductCatalog from './pages/ProductCatalog';
 import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
-import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
-// ... other imports
+import AdminDashboard from './pages/AdminDashboard';
+// other imports
 
-function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/catalog" element={<ProductCatalog />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/admin" element={<AdminDashboard />} />
-                {/* Add more routes as needed */}
-            </Routes>
-        </Router>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/catalog" element={<ProductCatalog />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        {/* other routes */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
