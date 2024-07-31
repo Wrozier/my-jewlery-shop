ffba83da88f86e065cfe8d88bb7b52ac9f80c807
William Rozier <wrozier@princesscruises.com>
2024-07-31 18:45:43 -0400

    Fix import paths for Navbar components

--- client/src/components/BreadCrums/Breadcrums.js
+++ client/src/components/BreadCrums/Breadcrums.js
@@ -2,13 +2,13 @@ import React from 'react'
 import './Breadcrums.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
-const Breadcrums = (props) => {
+const BreadCrums = (props) => {
   const {product} = props;
   return (
-    <div className='breadcrums'>
+    <div className='breadCrums'>
       HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name} 
     </div>
   )
 }
 
-export default Breadcrums
+export default BreadCrums
