e721076732359f28f9f44f86f1a3e4cffe72ec1d
William Rozier <wrozier@princesscruises.com>
2024-07-31 19:08:39 -0400

    Fix import paths for Navbar components 1

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+// /opt/build/repo/client/src/components/Navbar.js
+
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Navbar</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
